9ddd0f31eabd222c7983244707855091b24262e9
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-12-03 13:55:30 +0800

    feat: init function

---
+++ src/init.js
@@ -0,0 +1,23 @@
+import symbol from 'log-symbols';
+import chalk from 'chalk';
+import { join } from 'path';
+
+import { loadCmd } from "./utils"
+
+const init = async (appName) => {
+    const appPath = join(process.cwd(), appName);
+    const loadCmdCur = loadCmd(appPath);
+
+    try {
+        await loadCmdCur(`rm -rf ./.git && git init`, 'init git');
+        // await loadCmdCur(``);
+        // await loadCmd(`git init`, appPath)
+        await loadCmdCur(`npm install`, 'installing packaging')
+    } catch (err) {
+        console.log(symbol.error, chalk.red(`Init failed`));
+        console.log(symbol.error, err);
+    }
+}
+
+
+export default init;
