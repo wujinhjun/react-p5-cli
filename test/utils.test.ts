e701e1386c83fff83ba208956696274f2f5dbb7a
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-15 18:47:20 +0800

    test: the unit test for utils

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
-import { existFolder, downloadRepo } from "../src/utils";
+import { existFolder, downloadRepo, loadCommand } from "../src/utils";
+import * as path from "path";
 // const existFolder = require("../src/utils");
 
 describe("test utils", () => {
@@ -12,13 +13,27 @@ describe("test utils", () => {
   });
 
   test("test download function false git repo", async () => {
-    const repo: string = "https://github.com/wujinhjun/react-p5-scaff.git#main";
+    const repo = "https://github.com/wujinhjun/react-p5-scaff.git#main";
     await expect(downloadRepo(repo, "p5")).rejects.toBe(1);
   });
 
   test("test download function true git repo", async () => {
-    const repo: string =
-      "https://github.com/wujinhjun/react-p5-scaffold.git#main";
-    await expect(downloadRepo(repo, "p5")).resolves.toBe(0);
+    const testPath = path.join(process.cwd(), "folderForTest", "p5");
+    const repo = "https://github.com/wujinhjun/react-p5-scaffold.git#main";
+    await expect(downloadRepo(repo, testPath)).resolves.toBe(0);
   });
+
+  test("test the spawn succeed", async () => {
+    const testPath = path.join(process.cwd(), "folderForTest", "spawn1");
+    await expect(
+      loadCommand(testPath, "git", "initializing the git repository", ["init"])
+    ).resolves.toBe(0);
+  }, 25000);
+
+  test("test the spawn failed", async () => {
+    const testPath = path.join(process.cwd(), "folderForTest", "spawn2");
+    await expect(
+      loadCommand(testPath, "npm", "npm installing dependencies", ["install"])
+    ).rejects.toBe(2);
+  }, 15000);
 });
