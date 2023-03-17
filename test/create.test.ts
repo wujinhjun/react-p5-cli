5c3c8e1188d3a231b6069c0cc08d8af714ac15d9
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-03-17 17:28:39 +0800

    test: test for create

---
+++ test/create.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "@jest/globals";
+import create from "../src/create";
+import * as path from "path";
+import rimraf from "rimraf";
+
+describe("test create", () => {
+  test("test create when the folder existed", async () => {
+    await expect(create("test")).rejects.toThrow("folder existed");
+  });
+
+  test("test create when the remote is false", async () => {
+    await expect(
+      create("test1", "https://github.com/wujinhjun/react-p5")
+    ).rejects.toThrow("download failed");
+  });
+
+  test("test create truly", async () => {
+    await expect(
+      create("test1", "https://github.com/wujinhjun/react-p5-scaffold.git#main")
+    ).resolves.toBe(0);
+    rimraf(path.join(process.cwd(), "test1"));
+  }, 50000);
+});
